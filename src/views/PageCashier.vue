9ab44199fc15ffe0fc4871630fe1b8145ebde5db
felix <idn_felix.andrean>
2019-03-13 16:57:35 +0700

    remove transition when leave

--- src/views/PageCashier.vue
+++ src/views/PageCashier.vue
@@ -428,9 +428,6 @@ export default {
 	.slide-right-enter-active {
   	transition: all 0.5s ease;
 	}
-	.slide-right-leave-active {
-	  transition: all 0.5s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-	}
 	.slide-right-enter, .slide-right-leave-to
 	/* .slide-fade-leave-active below version 2.1.8 */ {
 	  transform: translateX(-100%);

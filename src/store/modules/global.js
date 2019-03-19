e677021856677ebb4319397fddf904cd3d1a3b71
felix <idn_felix.andrean>
2019-03-19 15:32:11 +0700

    add paycode params and implement to payment container

--- src/store/modules/global.js
+++ src/store/modules/global.js
@@ -4,36 +4,43 @@ export default {
 		dummyPaymentMethod: [
 			{
 				title: 'Uang Elektronik',
+				payCode: 'gopay',
 				logo: ['gopay-icon'],
 				promo: true,
 			}, 
 			{
 				title: 'Kartu Kredit & Debit',
+				payCode: 'cc',
 				logo: ['mastercard-color', 'visa-color', 'jcb-color'],
 				promo: true,
 			},
 			{
 				title: 'Transfer Virtual Account',
+				payCode: 'va',
 				logo: ['bca-icon', 'mandiri-icon', 'permata-icon'],
 				promo: true,
 			},
 			{
 				title: 'Bank Transfer',
+				payCode: 'bt',
 				logo: ['bca-icon', 'bni-icon', 'bri-icon'],
 				promo: true,
 			},
 			{
 				title: 'Cicilan Tanpa Kartu',
+				payCode: 'mf',
 				logo: ['kredivo-icon', 'kreditplus-icon', 'al-icon'],
 				promo: true,
 			},
 			{
 				title: 'Toko Ritel',
+				payCode: 'cs',
 				logo: ['alfamart-icon'],
 				promo: true,
 			},
 			{
 				title: 'Clickpay',
+				payCode: 'cp',
 				logo: ['mandiri-icon'],
 				promo: true,
 			},
